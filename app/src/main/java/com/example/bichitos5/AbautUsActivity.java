0fc2bb1e331417b66833eda2ce6c86307e01edc9
unknown <Admin@IHR80PBF161.continental.edu.pe>
2019-11-12 22:26:13 -0500

    final clase semana 5

---
+++ app/src/main/java/com/example/bichitos5/AbautUsActivity.java
@@ -0,0 +1,14 @@
+package com.example.bichitos5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbautUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abaut_us);
+    }
+}
--- app/src/main/java/com/example/bichitos5/MainActivity.java
+++ app/src/main/java/com/example/bichitos5/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.bichitos5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnProducts;
+    private Button btnAboUts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnProducts =(Button) findViewById(R.id.btnEnter);
+        btnAboUts = (Button) findViewById(R.id.btnAboUts);
+
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+    btnAboUts.setOnClickListener(new View.OnClickListener() {
+    public void onClick(View v){
+        //Log.i(Settings.INFO,"AbaUts us");
+        Intent i = new Intent(MainActivity.this, AbautUsActivity.class);
+        startActivity(i);
+
+    }
+});
     }
     @Override
   protected void onResume(){
